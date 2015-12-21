6f2a9cf502a1d6fe5ea43c1473f3e951ee488ebf
seiniku <J@keppers.org>
2015-12-20 20:37:27 -0600

    roughly working dash buttons

--- dash_watch.py
+++ dash_watch.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python
 """This script monitors for an IP address and flashes one light in the bedroom
 depending on which lights are available."""
-import os
+import subprocess
 import time
 from datetime import datetime, timedelta
-from phue import Bridge
+from qhue import Bridge
 import logging
 logging.basicConfig()
 
 
 class NetworkDevice(object):
     """ A network device to watch for """
-    def __init__(self, name, ip, color):
+    def __init__(self, name, ip, color, group_name):
         self.name = name
         self.ip = ip
         self.last_seen = datetime.now() - timedelta(days=1)
         self.color = color
+        self.group_name = group_name
 
     def get_ip(self):
         """ returns object's ip """
@@ -26,93 +27,54 @@ class NetworkDevice(object):
         return self.last_seen
 
 
-def find_a_light():
+def find_a_light(hue, group_name):
     """Finds an appropriate light and returns it."""
 
-    hue = Bridge('192.168.1.11')
-    # If running for the first time:
-    # press button on bridge and run with b.connect() uncommented
-    # b.connect()
-
-    lights = hue.get_light_objects('name')
-
-    # reachable is supposed to be an option but isn't..?
-    # if lights['Bedroom ceiling'].reachable:
-    if (hue.get_light('Bedroom ceiling')['state']['reachable'] and
-            hue.get_light('Bedroom ceiling')['state']['on']):
-        alert_light = lights['Bedroom ceiling']
-
-    elif (hue.get_light('Bedroom bed')['state']['reachable'] and
-            hue.get_light('Bedroom bed')['state']['on']):
-        alert_light = lights['Bedroom bed']
-
-    elif hue.get_light('Bedroom desk')['state']['reachable']:
-        alert_light = lights['Bedroom desk']
-
-    return alert_light
+    hue = Bridge('192.168.1.11', "newdeveloper")
+    # hue(devicetype="test user", username="newdeveloper", http_method="post")
+    groups = hue.groups()
+    for item in groups:
+        item_name = hue.groups(item)["name"]
+        if item_name == group_name:
+            lights = hue.groups(item)["lights"]
+            break
+    return lights
 
 
-def alert_the_light(alert_light, color):
-    """Flashes a light."""
-    number_of_flashes = 2
-    flash_delay = 1  # seconds
-    transition_time = 30  # deciseconds
+def should_turn_on(hue, lights):
+    """ returns true of the lights should be turned on """
+    should_power_on = True
+    for item in lights:
+        if hue.lights(item)["state"]["on"]:
+            should_power_on = False
+            break
+    return should_power_on
 
-    # Backup the lights configuration so that it can be stored again later.
-    on_backup = alert_light.on
-    hue_backup = alert_light.hue
-    xy_backup = alert_light.xy
-    brightness_backup = alert_light.brightness
-    transition_backup = alert_light.transitiontime
 
-    # If the light is already on, lets make it bright.
-    if alert_light.on:
-        min_brightness = 25
-        max_brightness = 240
-    else:
-        min_brightness = 2
-        max_brightness = 50
-
-    # Make sure the light is on, set it to red,
-    # and set the transition time.
-    alert_light.on = True
-    alert_light.brightness = min_brightness
-    alert_light.xy = color
-    alert_light.transitiontime = transition_time
-
-    # Flash it!
-    for _ in range(number_of_flashes):
-        alert_light.brightness = min_brightness
-        time.sleep(transition_time/10+flash_delay)
-        alert_light.brightness = max_brightness
-        time.sleep(transition_time/10+flash_delay)
-
-    # Set the light back to the original configuration.
-    if on_backup:
-        alert_light.hue = hue_backup
-        alert_light.xy = xy_backup
-        alert_light.brightness = brightness_backup
-        alert_light.transitiontime = transition_backup
-    else:
-        alert_light.transitiontime = None
-        alert_light.on = on_backup
+def toggle_lights(hue, lights, should_power_on):
+    """ toggles lights to should_power_on state """
+    print should_power_on
+    for item in lights:
+        hue.lights[item].state(on=should_power_on)
 
 
 def check_for_ip(device):
     """Pings an ip and returns network_device object
        with an updated last_seen property.
     """
-    ping = os.system("ping -c 1 -w 1 " + device.ip + " > /dev/null 2>&1")
+    ping = subprocess.call("ping -c 1 -w 1 " + device.ip + " > /dev/null 2>&1",
+                           shell=True)
+    hue = Bridge('192.168.1.11', "newdeveloper")
     if ping == 0:
         now = datetime.now()
-        if (now - device.last_seen) > timedelta(minutes=15):
-            light = find_a_light()
+        if (now - device.last_seen) > timedelta(seconds=15):
+            light = find_a_light(hue, device.group_name)
+            toggle_lights(hue, light, should_turn_on(hue, light))
             if light is not None:
                 print ("ALERT! - " +
                        now.strftime("%Y-%m-%d %H:%M") + " - " +
-                       device.name + " - " + light.name)
-                alert_the_light(light, device.color)
-        device.last_seen = now
+                       device.name)
+            device.last_seen = now
     return device
 
 
@@ -131,18 +93,36 @@ def main():
     colors['yellow'] = [0.4432, 0.5154]
     colors['violet'] = [0.3644, 0.2133]
 
-    jason = NetworkDevice("jason",
-                          "192.168.1.250",
-                          colors['firebrick'])
-    justin = NetworkDevice("justin",
-                           "192.168.1.251",
-                           colors['orchid'])
+    glad = NetworkDevice("glad",
+                         "glad.bad.wolf",
+                         colors['firebrick'],
+                         "Basement")
+
+    tide = NetworkDevice("tide",
+                         "tide.bad.wolf",
+                         colors['firebrick'],
+                         "Basement")
+
+    finish = NetworkDevice("finish",
+                           "finish.bad.wolf",
+                           colors['firebrick'],
+                           "Bedroom_all")
+
+    ziplock = NetworkDevice("ziplock",
+                            "ziplock.bad.wolf",
+                            colors['firebrick'],
+                            "Bedroom_all")
 
     # This is the main business loop, clearly.
     while True:
-        justin = check_for_ip(justin)
-        jason = check_for_ip(jason)
-        time.sleep(3)
-
+        try:
+            glad = check_for_ip(glad)
+            tide = check_for_ip(tide)
+            finish = check_for_ip(finish)
+            ziplock = check_for_ip(ziplock)
+            time.sleep(1)
+        except KeyboardInterrupt:
+            print "Quiting.."
+            exit()
 if __name__ == "__main__":
     main()
